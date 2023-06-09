105d9c90e0558447f86f1c68bc2ef32b88bc169e
Aniket <aniketdhokane2071@gmail.com>
2023-06-09 10:56:49 +0530

    news complete

--- src/Service/getNews.js
+++ src/Service/getNews.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export function getNews(){
+export function getNews(category){
   const API_KEY=`2441d9140ee14ff0aa831327117d0f3c`
-  const API_ENDPOINT=`https://newsapi.org/v2/top-headlines?country=us`
+  const API_ENDPOINT=`https://newsapi.org/v2/top-headlines?country=us&category=${category}`
 
   
         return axios.get(`${API_ENDPOINT}&apiKey=${API_KEY}`)
--- src/components/NewsData.js
+++ src/components/NewsData.js
@@ -1,31 +1,51 @@
 
 import { useEffect, useState } from 'react';
 import { getNews } from '../Service/getNews';
-
+import moment from 'moment'
 const NewsData = () => {
     const [newsData, setNewsData] = useState([])
+    const [selectOption, setSelectOption]=useState('')
     const getAllNews = async () => {
-        let data = await getNews();
+        let data = await getNews(selectOption);
         setNewsData(data.data.articles)
         // setNewsData(await getNews())
     }
+    const selectCategory=(event)=>{
+        setSelectOption(event.target.value);
+    }
+
     useEffect(() => {
         getAllNews()
-    }, [])
+    }, [selectOption])
     console.log(newsData?.data?.articles);
     return (
         <div className='main'>
             <h1>Voice news</h1>
+            <label for="cars">Choose a category:</label>
+            <div className='select'>
+                <select className='select-box' name="category" id="category" onChange={selectCategory} >
+                    <option value="general">General</option>
+                    <option value="health">Health</option>
+                    <option value="business">Business</option>
+                    <option value="sports">Sports</option>
+                </select>
+            </div>
             <div className='grid-main'>
                 {newsData?.map((news) => {
                     return (
                         <div className='grid-child'>
-                            
-                             <p> {news?.title} </p>
-                        
-                        
-                        
-                        
+                            <img className='news-image' src={news?.urlToImage} alt="" />
+                            <p className='news-title'> {news?.title} </p>
+                            <p className='news-content'> {news?.content} </p>
+                            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                <p className='news-author'>Author: {news?.author ? news?.author : 'Author name not available'} </p>
+                                <p className='news-date'>Date: {moment(news?.publishedAt).format('LL')}</p>
+                            </div>
+                            <a href={news?.url} target='_blank'>Read More..</a>
+
+
+
+
                         </div>
                     )
                 })}
